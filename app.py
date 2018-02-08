f38202c1792b2af5e364a3e7033b046b5e41fe4f
Emiel Kollof <emiel.kollof@gmail.com>
2018-02-08 13:31:22 +0100

    Python 3.6 compat Fix ini file parser

--- app.py
+++ app.py
@@ -1,43 +1,54 @@
 #!/usr/bin/env python
 
-import os
+import configparser
 import sys
-import requests
 
+import requests
 from flask import Flask, request, render_template, abort
 
 application = Flask(__name__)
-postUrl = ""
+
+config = configparser.ConfigParser()
+config.read('settings.ini')
+
+try:
+    post_url = config.get('global', 'posturl')
+except Exception as ex:
+    print('Something is wrong with config: {}'.format(ex))
+    sys.exit(1)
+
 
 @application.route("/")
 def hello():
-    return "I'm alive."
+    return "I'm alive.", 200
+
 
 @application.route("/webhook", methods=['POST'])
 def webhook():
     if request.method == 'POST':
         alert = requests.json()
-        omi = render_template('template.xml', 
+        omi = render_template('template.xml',
                               title='alert',
                               description=alert['commonAnnotation'],
                               severity=alert['status'],
                               node=alert['commonLabels'],
                               category='undefined',
-			      affectedCI='undefined'
-                            )
-       
-	print("Incoming JSON: %s\n" % alert)
-	print("Outgoing XML: %s\n" % omi)
+                              affectedCI='undefined'
+                              )
 
-	headers = {
-		'Content-type': 'text/xml',
-	}
+        print("Incoming JSON: %s\n" % alert)
+        print("Outgoing XML: %s\n" % omi)
 
-	response = requests.post(postUrl, headers, omi)
-        
-        return '', 200
+        headers = {
+            'Content-type': 'text/xml',
+        }
+
+        response = requests.post(post_url, headers, omi)
+
+        return '', response
     else:
-        abort(400)        
-    
+        abort(400)
+
+
 if __name__ == '__main__':
-    application.run(host="0.0.0.0",port=8080)
+    application.run(host="0.0.0.0", port=8080)
